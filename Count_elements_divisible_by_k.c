d4ab1f4bd990518db765d587aa0995b68e6465de
umamaheshrao <96564185+umamaheshrao@users.noreply.github.com>
2022-10-15 22:15:48 +0530

    #include <stdio.h>     int main()     {
            int i, j, a, n, number[100],c=0,k;
            scanf("%d%d", &n,&k);
            for (i = 0; i < n; ++i)
                scanf("%d", &number[i]);
             for(i=0;i<n;i++)
             {
                 if(number[i]%k==0)
                 {
                     c++;
                 }
             }
             printf("%d",c);
        }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main()
+    {
+ 
+        int i, j, a, n, number[100],c=0,k;
+        scanf("%d%d", &n,&k);
+        for (i = 0; i < n; ++i)
+            scanf("%d", &number[i]);
+         for(i=0;i<n;i++)
+         {
+             if(number[i]%k==0)
+             {
+                 c++;
+             }
+         }
+         printf("%d",c);
+    }

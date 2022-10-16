0731c57544b343cea43e93698333a1e540cddb93
umamaheshrao <96564185+umamaheshrao@users.noreply.github.com>
2022-10-16 15:15:27 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             if(i==3)             {                 printf("Pling");                 c++;             }             if(i==5)             {                 printf("Plang");                 c++;             }             if(i==7)             {                 printf("Plong");                 c++;             }         }     }     if(c==0)     {         printf("%d",n);     } }

---
+++ RainDrop.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            if(i==3)
+            {
+                printf("Pling");
+                c++;
+            }
+            if(i==5)
+            {
+                printf("Plang");
+                c++;
+            }
+            if(i==7)
+            {
+                printf("Plong");
+                c++;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("%d",n);
+    }
+}
